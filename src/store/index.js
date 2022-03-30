19b21fa3688b6bab856a8623c777319fcd386c1b
techws-dev <tanguy.dechiron@gmail.com>
2022-03-30 09:54:09 +0200

    add required Vue addons

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
+export default createStore({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
+  plugins: [createPersistedState]
+})
