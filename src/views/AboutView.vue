0ed0917be5ab67e9efd3e7a0db3d6325b8cef323
Tanguy Dechiron <tanguy.dechiron@gmail.com>
2022-04-08 10:55:35 +0200

    update about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,22 @@
 <template>
   <v-container
     fluid>
-    <h1>About</h1>
+    <h1 class="mb-5">About</h1>
 
-    <p>This open source project is developped with <strong>Vue 3</strong> and <strong>Vuetify</strong>.</p>
+    <p class="mb-2">
+      This web app helps organize your work on your projects with an <strong>agile board</strong>, used in methodologies like <strong>Kanban</strong> or <strong>Scrum</strong>.
+    </p>
 
-    <p>You can look at the code on <a href="https://github.com/techws-dev/agile-board-app" target="_blank" class="text-decoration-none text-blue">github</a>.</p>
+    <p class="mb-2">
+      All datas are saved in the locale storage of the browser.
+    </p>
+
+    <p class="mb-2">
+      This open source project is developped with <strong>Vue 3</strong> and <strong>Vuetify</strong>.
+    </p>
+
+    <p class="mb-2">
+      You can look at the code on <a href="https://github.com/techws-dev/agile-board-app" target="_blank" class="text-decoration-none text-blue">github</a> (If you like it, please consider leaving a star).
+    </p>
   </v-container>
 </template>
