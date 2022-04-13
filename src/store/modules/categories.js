24de5b1266bf892bccf851b28891a0769abe7da9
Tanguy Dechiron <tanguy.dechiron@gmail.com>
2022-04-13 09:38:20 +0200

    add categories and tickets sorting

--- src/store/modules/categories.js
+++ src/store/modules/categories.js
@@ -1,4 +1,6 @@
 
+import { v4 as uuidv4 } from 'uuid'
+
 // eslint-disable-next-line no-unused-vars
 const DEFAULT_CATEGORIES = [
   {
@@ -34,16 +36,37 @@ export default {
     },
   },
   mutations: {
-    createDefaultCategoriesForProject(state, id) {
-      console.log(id)
+    createDefaultCategoriesForProject(state, projectId) {
       DEFAULT_CATEGORIES.forEach(category => {
+        let id = uuidv4()
         state.categories.push({
+          id,
           key: category.key,
           label: category.label,
           order: category.order,
-          projectId: id
+          projectId
         })
       });
+    },
+
+    move(state, {id, projectId, oldIndex, newIndex}) {
+      console.log('category move')
+      state.categories.filter(category => category.projectId === projectId && category.id !== id)
+      .forEach(category => {
+        if (category.order > oldIndex) {
+          category.order -= 1
+        }
+      })
+    
+      state.categories.filter(category => category.projectId === projectId && category.id !== id)
+      .forEach(category => {
+        if (category.order >= newIndex) {
+          category.order += 1
+        }
+      })
+
+      let i = state.categories.map(category => category.id).indexOf(id)
+      state.categories[i].order = newIndex
     },
 
     deleteFromProject(state, id) {
@@ -51,8 +74,12 @@ export default {
     },
   },
   actions: {
-    createDefaultCategoriesForProject(context, id) {
-      context.commit('createDefaultCategoriesForProject', id)
+    createDefaultCategoriesForProject(context, projectId) {
+      context.commit('createDefaultCategoriesForProject', projectId)
+    },
+
+    move(context, {id, projectId, oldIndex, newIndex}) {
+      context.commit('move', {id, projectId, oldIndex, newIndex})
     },
 
     deleteFromProject(context, id) {
