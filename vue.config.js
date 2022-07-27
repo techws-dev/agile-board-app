6148772c05cf5a2339bf7640f88ea8dbfadec750
Tanguy Dechiron <tanguy.dechiron@gmail.com>
2022-07-27 11:08:45 +0200

    fix public path for production

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,9 @@ module.exports = defineConfig({
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
-  }
+  },
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/agile-board-app/'
+    : '/'
 })
